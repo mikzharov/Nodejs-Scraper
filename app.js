d63ee232621db5911f90081fc7126517e811ca57
Misha Zharov <mishazharov1@gmail.com>
2017-08-25 15:29:54 -0400

    Added outbound POST request ability

--- app.js
+++ app.js
@@ -5,17 +5,20 @@ var https = require('https');//For TLS
 var http = require('http');//For sending and receiving  HTTP requests
 var url = require('url');//For parsing urls
 var path = require('./config/paths.js');//API scrapping info
+var querystring = require('querystring');//For post requests
 
 var app = express();//Initializes express
 
 //Loads paths to scrap
-for (var i = 0; i < path.api_objects.get.length; i++) {
+path.api_objects.get.forEach( function(api_object){
     //Sets options for HTTP request
     var option_port;//Outbound port to connect to
     var outbound_security;//Whether to use HTTPS or HTTP
 
+    api_object.outbound_method = api_object.outbound_method.toUpperCase();
+
     //Makes sure port and protocol are correct
-    const parsed_url = url.parse(path.api_objects.get[i].outbound_url);
+    const parsed_url = url.parse(api_object.outbound_url);
     if (parsed_url.protocol) {
         if (parsed_url.protocol == 'https:') {
             option_port = 443;
@@ -36,48 +39,67 @@ for (var i = 0; i < path.api_objects.get.length; i++) {
         option_port = 80;
     }
     var option_path = "";
-    if(parsed_url.path){
+    if (parsed_url.path) {
         option_path += parsed_url.path;
     }
-    if(parsed_url.hash){
+    if (parsed_url.hash) {
         option_path += parsed_url.hash;
     }
     var options = {
         host: parsed_url.hostname,//Sets hostname
         port: option_port,//Sets port to connect to
         path: option_path,//Sets the pathname to fetch
-        path_parameters:'',
-        method: path.api_objects.get[i].outbound_method,//Sets the method to use
-        headers: path.api_objects.get[i].outbound_headers,
+        method: api_object.outbound_method,//Sets the method to use
+        headers: {},//Default header value of nothing
     };
-    if(parsed_url.auth){
+    //Sets the body if it exists
+    if(api_object.outbound_body){
+        if(typeof api_object.outbound_body === 'object'){
+            options.body = querystring.stringify(api_object.outbound_body);
+        }else{
+            options.body = api_object.outbound_body;
+        }
+    }
+    //Sets the headers if they exist
+    if(api_object.outbound_headers){
+        options.headers = api_object.outbound_headers;
+    }
+    if (parsed_url.auth) {
         var auth_header = 'Basic ' + new Buffer(parsed_url.auth).toString('base64');
         options.headers.Authorization = auth_header;
     }
-    console.log(options.headers)
 
-    app.get(path.api_objects.get[i].inbound_url, function (inbound_req, inbound_res) {
+
+    app.get(api_object.inbound_url, function (inbound_req, inbound_res) {
         //Replace outbound URL parameters with inbound_req parameters
 
-        Object.keys(inbound_req.params).forEach(function (key) {//"p:" for path parameters
-            if(key)
-            options.path = options.path.replace("p:" + key, inbound_req.params[key]);
+        Object.keys(inbound_req.params).forEach(function (key) {//"param:" for path parameters
+            if (key) {
+                options.path = options.path.replace("param:" + key, inbound_req.params[key]);
+                if (options.body)
+                    options.body = options.body.replace("param:" + key, inbound_req.params[key]);
+            }
         });
-        Object.keys(inbound_req.query).forEach(function (key) {//"q:" for query parameters
-            if(key)
-            options.path = options.path.replace("q:" + key, inbound_req.query[key]);
+        Object.keys(inbound_req.query).forEach(function (key) {//"query:" for query parameters
+            if (key) {
+                options.path = options.path.replace("query:" + key, inbound_req.query[key]);
+                if (options.body){
+                    options.body = options.body.replace("query:" + key, inbound_req.query[key]);
+                    options.headers['Content-Length'] = Buffer.byteLength(options.body);
+                }
+            }
         });
         //Same object for HTTP or HTTPS connection depending on switch statement below
         var outbound_req;
         //The result of the request
-        var body = "";
+        var result_body = "";
         //The callback which parses the request
         function callback(outbound_res) {
             outbound_res.on('data', function (d) {
-                body += d;
+                result_body += d;
             });
             outbound_res.on('end', function () {
-                inbound_res.send(body);
+                inbound_res.send(result_body);
             });
         }
         //Decides whether to use HTTP or HTTPS
@@ -89,12 +111,13 @@ for (var i = 0; i < path.api_objects.get.length; i++) {
                 outbound_req = http.request(options, callback);
                 break;
         }
-        outbound_req.end();//Ends request. This is required by express
         outbound_req.on('error', function (e) {//If there are errors it should output them as json objects.
-            console.error(e);//TODO
+            inbound_res.send(e);
         });
+        outbound_req.write(options.body);//For post requests
+        outbound_req.end();//Ends request. This is required by express
     })
-}
+});
 
 //Server variables
 var server_https;
--- config/paths.js
+++ config/paths.js
@@ -6,9 +6,31 @@ path.api_objects.get = [];
 path.api_objects.post =[];
 
 path.api_objects.get.push({
-    outbound_url:"https://me:too@www.nationstates.net/nation=p:id",//The url to scrap from
+    //param: = path parameters; ex. https://example.com/path/node/564
+    //query: = query parameters; ex. ?test=5&me=6
+    outbound_url:"https://me:too@www.nationstates.net/nation=param:id",//The url to scrap from
     outbound_method:"GET",//The method to use when scrapping
     outbound_headers:{},
+    outbound_body:{},
     inbound_url:"/nation/:id",//The path that should be used to start scrapping on this server
 });
+body = "param=query:me";
+path.api_objects.get.push({
+    //param: = path parameters; ex. https://example.com/path/node/564
+    //query: = query parameters; ex. ?test=5&me=6
+    outbound_url:"https://requestb.in/1h5vtsa1",//The url to scrap from
+    outbound_method:"POST",//The method to use when scrapping
+    outbound_body: body,
+    outbound_headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    inbound_url:"/posttest",//The path that should be used to start scrapping on this server
+});
 module.exports = path;
+/*
+outbound_body: body,
+outbound_headers: {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Content-Length': Buffer.byteLength(body)
+},
+*/
